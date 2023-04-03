3874a58f5861e01513616db32fb713de991616b4
Antonio Masetti <antoniopsmasetti@gmail.com>
2023-04-02 21:58:11 -0300

    Mudança Item Box (Ainda Incompleto)

--- Assets/Karting/Scripts/ItemBox.cs
+++ Assets/Karting/Scripts/ItemBox.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ItemBox : MonoBehaviour
 {
+    int Index;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +16,42 @@ public class ItemBox : MonoBehaviour
     {
         
     }
+    /* IEnumerator OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "ItemBox")
+        {
+            other.gameObject.GetComponent<BoxCollider>().enabled = false;
+            other.transform.GetChild(1).GetChild(1).GetComponent<SkinnedMeshRenderer>().enabled = false;
+            for (int i = 1; i < 3; I++)
+            {
+                other.transform.GetChild(2).GetChild(i).GetComponent<SkinnedMeshRenderer>().enabled = false;
+            }
+            other.gameObject.GetComponent<Animator>().SetBool("Enlage", false);
+            StartCoroutine(getItem());
+            ItemUIAnim.SetBool("ItemIn", true);
+            ItemUIScroll.SetBool("Scroll", true);
+
+            yield return new WaitForSeconds (1);
+            Other.transform.GetChild(1).GetComponent<SkinnedMeshRenderer>().enabled = true;
+            for (int i = 1; i < 3; I++)
+            {
+                other.transform.GetChild(2).GetChild(i).GetComponent<SkinnedMeshRenderer>().enabled = true;
+            }
+            other.gameObject.GetComponent<Animator>().SetBool("Enlarge", true);
+            other.gameObject.GetComponent<BoxCollider>().enabled = true;
+        }
+    }
+
+    public IEnumerator getItem()
+    {
+        if (!hasItem)
+        {
+            Index = Random.Range(0, itemGameobjects.length);
+            yourSprite.sprite = itemSprites[index];
+            yield return new WaitForSeconds(4f);
+
+            itemGameobjects[index].SetActive(true);
+            hasItem = true;
+        }
+    }*/
 }
