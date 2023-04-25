60a1d49ca2ce202aec6254d6bd18e012cdf41c4f
Antonio Masetti <antoniopsmasetti@gmail.com>
2023-04-24 21:24:48 -0300

    Multiplayer (Incompleto)

---
+++ Assets/Karting/Scripts/KartSystems/Inputs/KeyboardInput1.cs
@@ -0,0 +1,21 @@
+﻿/*using UnityEngine;
+
+namespace KartGame.KartSystems {
+
+    public class KeyboardInput1 : BaseInput
+    {
+        public string TurnInputName1 = "Horizontal1";
+        public string AccelerateButtonName1 = "Accelerate1";
+        public string BrakeButtonName1 = "Brake1";
+
+        public override InputData GenerateInput1() {
+            return new InputData
+            {
+                Accelerate = Input.GetButton(AccelerateButtonName1),
+                Brake = Input.GetButton(BrakeButtonName1),
+                TurnInput = Input.GetAxis("Horizontal1")
+            };
+        }
+    }
+}
+*/
---
+++ Assets/Karting/Scripts/SpawnCar1.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+using KartGame.KartSystems;
+
+public class SpawnCar1 : MonoBehaviour
+{
+    /*public Transform trans;
+    public GameObject gameObj;
+    public GameObject[] allVehicles;
+    public CinemachineVirtualCamera cinemachineCamera; //m_LookAt, m_Follow
+    public GameFlowManager gameFlowManager; //playerKart
+    // Start is called before the first frame update
+    void Awake()
+    {
+        int currentVehicle = VehicleSelector.currentVehicle;
+
+        GameObject carroPlayer = Instantiate(allVehicles[currentVehicle], trans.position, Quaternion.identity);
+        gameFlowManager.playerKart = carroPlayer.GetComponent<ArcadeKart>();
+        cinemachineCamera.m_LookAt = carroPlayer.transform;
+        cinemachineCamera.m_Follow = carroPlayer.transform;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }*/
+}
