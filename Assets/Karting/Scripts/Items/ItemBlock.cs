3ffb9e2eac1dcc24c4646e11855f1c186a3a7395
Antonio Masetti <antoniopsmasetti@gmail.com>
2023-04-27 21:06:56 -0300

    Portal Completo

--- Assets/Karting/Scripts/Items/ItemBlock.cs
+++ Assets/Karting/Scripts/Items/ItemBlock.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class ItemBlock : MonoBehaviour
 {
+   public GameObject[] allLights;
+   public static int currentLight = 1;
+
    private void OnTriggerEnter(Collider other) 
    {
+        
         if(other.tag == "Player")
         {
-            if(other.GetComponent<KartItem>().HeldItem == -1 && other.GetComponent<KartItem>().CanPickup)
-            {
+            allLights[currentLight].SetActive(false);
+            currentLight = currentLight % allLights.Length;
+
+            StartCoroutine(WaitSeconds());
+            
+            KartItem currentItem = other.GetComponent<KartItem>();
+
+            if(currentItem != null) { 
+                if(currentItem.HeldItem == -1 && currentItem.CanPickup)
+                {
                 other.GetComponent<KartItem>().StartPickup();
+                }
             }
         }
+
+    IEnumerator WaitSeconds()
+    {
+        allLights[currentLight].SetActive(true);
+        
+        yield return new WaitForSeconds(3);
+
+        allLights[currentLight].SetActive(false);
+    }
+
    }
 
 }
